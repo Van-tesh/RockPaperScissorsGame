ac8f99ba464b7f78924c116e2c503e22d3bbb37c
Van-tesh <smalletvicky@gmail.com>
2023-03-13 20:07:50 +0300

    added a while loop

--- main.py
+++ main.py
@@ -1,57 +1,64 @@
+while True:
+  rock = r'''  
+      _______
+  ---'   ____)  
+        (_____)  
+        (_____)  
+        (____)
+  ---.__(___)  
+  '''
 
-rock = r'''  
-    _______
----'   ____)  
-      (_____)  
-      (_____)  
-      (____)
----.__(___)  
-'''
+  paper = r'''  
+      _______
+  ---'   ____)____  
+            ______)  
+            _______)  
+          _______)
+  ---.__________)  
+  '''
 
-paper = r'''  
-    _______
----'   ____)____  
-          ______)  
-          _______)  
-         _______)
----.__________)  
-'''
+  scissors = r'''  
+      _______
+  ---'   ____)____  
+            ______)  
+        __________)  
+        (____)
+  ---.__(___)  
+  '''  
 
-scissors = r'''  
-    _______
----'   ____)____  
-          ______)  
-       __________)  
-      (____)
----.__(___)  
-'''  
+  import random
 
-import random
+  user_choice= int(input("Please choose 0 for rock, 1 for paper and 2 for scissor\n\n"))
 
-user_choice= int(input("Please choose 0 for rock, 1 for paper and 2 for scissor\n\n"))
+  if user_choice==0:
+    print("You chose\n "+rock)
+  elif user_choice==1:
+    print("You chose\n "+paper)
+  elif user_choice==2:
+    print("You \n "+scissors)
+      
 
-if user_choice==0:
-  print("You chose\n "+rock)
-elif user_choice==1:
-  print("You chose\n "+paper)
-elif user_choice==2:
-  print("You \n "+scissors)
+  inputs= [rock, paper, scissors]
+  random_computer_choice= random.randint(0,2)
+
+  print(f"the computer chose\n {inputs[random_computer_choice]}")
+
+  if user_choice>=3 or user_choice<0:
+    print("Invalid Input, You Lose!")
+  elif user_choice==0 and random_computer_choice== 2:
+    print("You win!")
+  elif random_computer_choice == 0 and user_choice == 2:
+    print("You lose!")
+  elif user_choice== random_computer_choice:
+    print("Its a draw!")
+  elif random_computer_choice > user_choice:
+    print("You Lose!")
   
+  elif user_choice > random_computer_choice:
+    print("You Win!")
+  elif user_choice>=3 or user_choice<0:
+    print("Invalid Input, You Lose!")
   
-inputs= [rock, paper, scissors]
-random_computer_choice= random.randint(0,2)
-
-print(f"the computer chose\n {inputs[random_computer_choice]}")
-
-if user_choice==0 and random_computer_choice== 2:
-  print("You win!")
-elif user_choice== random_computer_choice:
-  print("Its a draw!")
-elif random_computer_choice > user_choice:
-  print("You Lose!")
-elif random_computer_choice == 0 and user_choice == 2:
-  print("You lose!")
-elif user_choice > random_computer_choice:
-  print("You Win!")
-else:
-  print("Invalid Input, You Lose!")
+  play_again = input("Play again? (y/n): ")
+  if play_again.lower() != "y":
+    break
